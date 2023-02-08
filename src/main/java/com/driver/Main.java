81c1b30298751d53b18815b437b5189245536484
Angad <anilterapalli@gmail.com>
2023-02-08 12:14:42 +0530

    encapsulation assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+    RWOnly rwobj = new RWOnly();
+    rwobj.setName("Anil");
+    System.out.println(rwobj.getName());
+  }
 }
